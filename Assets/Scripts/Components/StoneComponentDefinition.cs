a0f038f4ff2cd29afe4027307ae8ab596b06caa2
Ariel Coppes <ariel.coppes@gmail.com>
2024-04-15 10:55:49 -0300

    show runes in stones

---
+++ Assets/Scripts/Components/StoneComponentDefinition.cs
@@ -0,0 +1,42 @@
+﻿using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+
+namespace Components
+{
+    public struct StoneComponent : IEntityComponent
+    {
+        public int rune;
+        public bool on;
+        public int order;
+        public Sprite[] runeSprites;
+    }
+    
+    public class StoneComponentDefinition : ComponentDefinitionBase
+    {
+        public int rune;
+
+        public Sprite[] runeSprites;
+        
+        public override string GetComponentName()
+        {
+            return nameof(StoneComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            if (!world.HasComponent<StoneComponent>(entity))
+            {
+                world.AddComponent(entity, new StoneComponent()
+                {
+                    rune = rune,
+                    runeSprites = runeSprites
+                });
+            }
+            else
+            {
+                ref var stone = ref entity.Get<StoneComponent>();
+                stone.rune = rune;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/StonesRuneSystem.cs
@@ -0,0 +1,26 @@
+﻿using Components;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Systems
+{
+     public class StonesRuneSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<ModelComponent, StoneComponent>, Exc<DisabledComponent>> filter = default;
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in filter.Value)
+            {
+                ref var model = ref filter.Pools.Inc1.Get(e);
+                ref var stone = ref filter.Pools.Inc2.Get(e);
+
+                var runeRenderer = model.instance.transform.Find("Rune").GetComponent<SpriteRenderer>();
+                runeRenderer.sprite = stone.runeSprites[stone.rune];
+            }
+        }
+    }
+}
