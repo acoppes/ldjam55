e71f31c5053012d63dbacf6746d3109679b7b284
Ariel Coppes <ariel.coppes@gmail.com>
2024-04-14 22:32:03 -0300

    adding summoner

---
+++ Assets/Scripts/Controllers/SummonerController.cs
@@ -0,0 +1,52 @@
+﻿using Game;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+
+namespace Controllers
+{
+    public class SummonerController : ControllerBase, IUpdate, IInit, IStateChanged
+    {
+        public void OnInit(World world, Entity entity)
+        {
+           
+        }
+        
+        public void OnUpdate(World world, Entity entity, float dt)
+        {
+            var input = entity.Get<InputComponent>();
+            ref var movement = ref entity.Get<MovementComponent>();
+            ref var animations = ref entity.Get<AnimationComponent>();
+
+            movement.movingDirection = input.direction3d();
+
+            if (movement.movingDirection.sqrMagnitude > 0.1f)
+            {
+                if (!animations.IsPlaying("Walk-0"))
+                {
+                    animations.Play("Walk-0");
+                }
+            }
+            else
+            {
+                if (!animations.IsPlaying("Idle-0"))
+                {
+                    animations.Play("Idle-0");
+                }
+            }
+        }
+
+
+        public void OnEnterState(World world, Entity entity)
+        {
+           
+        }
+
+        public void OnExitState(World world, Entity entity)
+        {
+          
+        }
+    }
+}
